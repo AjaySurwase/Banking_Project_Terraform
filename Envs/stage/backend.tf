94ef9bf3974a4d45aa292ecabae7bad17caa3f3b
AjaySurwase <surwaseajay07@gmail.com>
2025-06-18 12:11:58 +0530

    Update backend.tf

--- Envs/stage/backend.tf
+++ Envs/stage/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hcb-bank-terraform-state"
+    bucket = "hcb-bank-terraform-state18"
     key    = "stage/terraform.tfstate"
     region = "us-east-1"
   }
